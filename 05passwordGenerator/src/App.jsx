68f293814b7a359d2963036eac93971bd24d04f8
Utkarsh Kumar <kumar6.utkarsh@gmail.com>
2024-09-01 02:46:02 +0530

    Made some minor changes and fixed copy icon path to simple path

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,14 +1,13 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
-import './App.css';
+import "./App.css";
 
 function App() {
   const [length, setLength] = useState(8);
   const [isNumericAllowed, setIsNumericAllowed] = useState(false);
   const [isCharactersAllowed, setIsCharactersAllowed] = useState(false);
   const [password, setPassword] = useState("");
-  const [isCopied, setIsCopied] = useState(false); // State to track if the button is clicked
-  const [isModalOpen, setIsModalOpen] = useState(false); // State to control modal visibility
-
+  const [isCopied, setIsCopied] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const passwordRef = useRef(null);
 
@@ -19,26 +18,27 @@ function App() {
     if (isNumericAllowed) str += "0123456789";
     if (isCharactersAllowed) str += "!@#$%^&*-_+=[]{}~`";
 
-    for (let i = 1; i < length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1);
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length);
       pass += str.charAt(char);
     }
     setPassword(pass);
-  }, [length, isNumericAllowed, isCharactersAllowed, setPassword]);
+  }, [length, isNumericAllowed, isCharactersAllowed]);
 
   const copyPasswordToClipboard = useCallback(() => {
-    passwordRef?.current?.select();
-    passwordRef?.current?.setSelectionRange(0, length);
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, length);
+
     window.navigator.clipboard.writeText(password);
-    setIsCopied(true); // Set the button to active state
-    setIsModalOpen(true); // Show the modal
+
+    setIsCopied(true);
+    setIsModalOpen(true);
 
     setTimeout(() => {
-      setIsCopied(false); // Revert back to default state after 2 seconds
-      setIsModalOpen(false); // Hide the modal after 2 seconds
+      setIsCopied(false);
+      setIsModalOpen(false);
     }, 1500);
-
-  }, [password, length]);
+  }, [password]);
 
   useEffect(() => {
     passwordGenerator();
@@ -46,13 +46,13 @@ function App() {
 
   return (
     <div className="bg-rainbow-animation h-screen w-screen flex items-center justify-center pt-10">
-      <div className='w-full max-w-6xl mx-auto shadow-lg rounded-2xl px-6 py-10  text-lime-500 bg-gray-800'>
+      <div className='w-full max-w-6xl mx-auto shadow-lg rounded-2xl px-6 py-10 text-lime-500 bg-gray-800'>
         <h1 className='text-5xl text-center my-6 font-bold'>Password Generator</h1>
         <div className="flex shadow rounded-xl overflow-hidden mb-6">
           <input
             type='text'
             value={password}
-            className='outline-none w-screen py-3 px-5 text-2xl'
+            className='outline-none w-full py-3 px-5 text-2xl'
             placeholder='Password'
             readOnly
             ref={passwordRef}
@@ -64,12 +64,12 @@ function App() {
             {isCopied ? (
               <div className="text-white flex items-center gap-x-2">
                 Copied!
-                <img src="../public/copied.svg" alt="Copied Icon" className="size-6" />
+                <img src='/copied.svg' alt="Copied Icon" className="w-6 h-6" />
               </div>
             ) : (
               <>
                 Copy
-                <img src="../public/copy.svg" alt="Copied Icon" className="size-6" />
+                <img src='/copy.svg' alt="Copy Icon" className="w-6 h-6" />
               </>
             )}
           </button>
@@ -91,7 +91,7 @@ function App() {
               type="checkbox"
               id='numeric'
               checked={isNumericAllowed}
-              onChange={() => setIsNumericAllowed(!isNumericAllowed)}
+              onChange={() => setIsNumericAllowed(prev => !prev)}
             />
             <label htmlFor="numeric">Numbers</label>
           </div>
@@ -100,19 +100,18 @@ function App() {
               type="checkbox"
               id='characters'
               checked={isCharactersAllowed}
-              onChange={() => setIsCharactersAllowed(!isCharactersAllowed)}
+              onChange={() => setIsCharactersAllowed(prev => !prev)}
             />
             <label htmlFor="characters">Characters</label>
           </div>
         </div>
       </div>
-      {/* Modal */}
       {isModalOpen && (
-        // <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
-        <div className='bg-slate-600 p-4 rounded-lg shadow-lg fixed bottom-20'>
-          <p className='text-lg text-white font-semibold'>Copied to clipboard!</p>
+        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
+          <div className='bg-slate-600 p-4 rounded-lg shadow-lg'>
+            <p className='text-lg text-white font-semibold' aria-live="assertive">Copied to clipboard!</p>
+          </div>
         </div>
-        // </div>
       )}
     </div>
   );
